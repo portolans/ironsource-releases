f804094199d6383e29acde15e06b3c4101891862
Nick Entin <nick@portola.ai>
2026-01-15 01:42:24 -0800

    Include PrivacyInfo.xcprivacy as resource

--- Package.swift
+++ Package.swift
@@ -13,8 +13,16 @@ let package = Package(
         ),
     ],
     targets: [
-        .binaryTarget(
+        .target(
             name: "IronSource",
+            dependencies: ["IronSourceBinary"],
+            resources: [.copy("Resources/PrivacyInfo.xcprivacy")],
+            linkerSettings: [
+                .unsafeFlags(["-ObjC"]), // Required to support IronSource's use of Objective-C extensions on common types.
+            ],
+        ),
+        .binaryTarget(
+            name: "IronSourceBinary",
             url: "https://github.com/portolans/ironsource-releases/releases/download/9.2.0/IronSource.xcframework.zip",
             checksum: "4ecde8ef03d7274589fdc2681a22bb80159a5e5d3cb9c47d778039e8bb0e58cb",
         ),
